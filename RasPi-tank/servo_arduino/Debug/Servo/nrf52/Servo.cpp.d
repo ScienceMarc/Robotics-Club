b3da9af2f647dadb933df409d8358b49474c4230
Marc L <sciencemarc@github.com>
2018-09-17 21:31:33 +0200

    VS debug info

---
+++ RasPi-tank/servo_arduino/Debug/Servo/nrf52/Servo.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\MarcL\AppData\Local\Temp\VMBuilds\servo_arduino\nano_atmega328old\Debug\Servo\nrf52\Servo.cpp.o: \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Servo\src\nrf52\Servo.cpp
---
+++ RasPi-tank/servo_arduino/Release/Servo/stm32f4/Servo.cpp.d
@@ -0,0 +1,2 @@
+C:\Users\MarcL\AppData\Local\Temp\VMBuilds\servo_arduino\nano_atmega328old\Release\Servo\stm32f4\Servo.cpp.o: \
+ C:\Program\ Files\ (x86)\Arduino\libraries\Servo\src\stm32f4\Servo.cpp
