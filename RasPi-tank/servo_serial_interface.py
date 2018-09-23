164444c9be6d1a2904b58a806211c9789c912272
Marc L <sciencemarc@github.com>
2018-09-23 19:30:11 +0200

    Reverted python program to be linux like to work with raspberry pi

--- RasPi-tank/servo_serial_interface.py
+++ RasPi-tank/servo_serial_interface.py
@@ -1,11 +1,11 @@
 import serial
 import time
-ser = serial.Serial('COM3', 9600)
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 print("Opened port at 9600 baud")
 time.sleep(1.5) #Wait for arduino to reset
-ser.write('*'.encode())
-ser.write('9'.encode())
-ser.write('0'.encode())
+ser.write('*'.encode()) #identifier
+ser.write('9'.encode()) #Value
+ser.write('0'.encode()) #Value
 
 ser.close()
 print("Closed port")
