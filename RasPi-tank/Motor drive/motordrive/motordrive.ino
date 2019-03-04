392ff9ef573e2bc103bcf55fa1d69b3f6e10df27
ScienceMarc <marc.lavergne@hotmail.com>
2019-03-04 16:06:22 +0000

    Added motor driving sketch

---
+++ RasPi-tank/Motor drive/motordrive/motordrive.ino
@@ -0,0 +1,14 @@
+#define RIGHTFORWARDS 9
+#define RIGHTBACKWARDS 8
+#define LEFTFORWARDS 11
+#define LEFTBACKWARDS 10
+
+void setup() {
+  // put your setup code here, to run once:
+  digitalWrite(RIGHTFORWARDS, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
